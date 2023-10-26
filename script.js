ad8e9b38b9b0fbfd8f09a8ee92ff9c8fa2ef6e4c
JayMacapagal <115454601+JayMacapagal@users.noreply.github.com>
2023-10-26 09:59:45 +0800

    0.4

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ hamburger.addEventListener('click', ()=>{
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
 });
+const cardControllers = document.querySelectorAll("[data-card-controller]");
+
+cardControllers.forEach(controller => {
+  controller.addEventListener("click", (e) => {
+    const card = e.currentTarget.parentElement.parentElement;
+    const isVisible = card.dataset.visible;
+
+    if (isVisible === "false") {
+      card.setAttribute("data-visible", true);
+    } else {
+      card.setAttribute("data-visible", false);
+    }
+  })
+})
